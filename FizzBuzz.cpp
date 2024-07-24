21f2526f85340e27c72460b3d1f3c7c3a7073d00
Hywandra <163301851+Hywandra@users.noreply.github.com>
2024-07-25 00:44:53 +0100

    tinny steps, tinny exercises
    I'll just share, for now, the exercises that I've been doing.
    It has been a really hard journey... Must keep up with something!
    
    #codecademy #cplusplus #visualstudio #wtfamidoing #newbie #programlearner #codinglearner #LiveAndLetLive #portugal

---
+++ FizzBuzz.cpp
@@ -0,0 +1,53 @@
+// FizzBuzz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	
+	for ( int i=1; i<=100; i++)
+
+	{
+	
+	if (i%15==0)
+		{
+			cout << "\n\tFizzBuzz!\n";
+		}
+		else if (i%5==0)
+		{
+			cout << "\n\tBUZZ!\n";
+		}
+		else if ( i % 3 == 0)
+		{
+			cout << "\n\tFIZZ!\n";
+		}
+		else
+		{
+			cout << "\n\t" << i << "\n";
+		}
+		
+	}
+
+
+	/*if (i == m3)
+		{
+			cout << "\n\tFIZZ!\n";
+		}
+		else if (i==m5)
+		{
+			cout << "\n\tBUZZ!\n";
+		}
+		else if (i==m35)
+		{
+			cout << "\n\tFizzBuzz!\n";
+		}
+		else
+		{
+			cout << "\n\t" << i << "\n";
+		}
+		*/
+}
+
